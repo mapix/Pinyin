cc5049747a5d1ad8d92429bb65ee63dcbddeb2d6
mapix <mapix.me@gmail.com>
2012-07-12 13:24:40 +0800

    误删了开发的脚本， 这次版本管理一下

---
+++ zhuyin.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import os
+import urllib2, urllib
+import lxml.html.soupparser as soupparser
+
+url = "http://py.kdd.cc/unicode/"
+data = { "zy":"1", "dy":"red", "ps":"gray", "zs":"blue", "kd":"80" }
+
+def crawler_wz_pinyin(wz):
+    data['wz'] = wz.encode('utf-8')
+    f = urllib2.urlopen(url=url, data=urllib.urlencode(data))
+    dom = soupparser.fromstring(f.read())
+    py_wz = dom.xpath('//*[@id="v"]/font/font[1]')[0].text_content()
+    return py_wz.strip().split()
+
+def process_w(w):
+    fout_l = {}
+    for wz in open('duoyinzi/'+w):
+        wz = wz.strip().decode('utf-8')
+        py_wz = crawler_wz_pinyin(wz)
+        py_w = py_wz[wz.index(w)]
+        if not py_w in fout_l:
+            fout_l[py_w] = open("zhuyin/"+w.encode('utf-8') \
+                                +"."+py_w.encode('utf-8'), 'w')
+        #fout_l[py_w].write(wz+"#"+"@".join(py_wz) + '\n')
+
+    for f in fout_l:
+        f.flush()
+        f.close()
+
+def zhuyin(path):
+    for w in os.listdir(path): 
+        process_w(w.decode('utf-8'))
+        print w
+
+
+if __name__ == "__main__":
+    zhuyin("duoyinzi")
+    print "DONE"
