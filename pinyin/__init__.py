36f3fcae28425ac7f5f9bf81da43530a8c3b3f9c
luoweifeng <luoweifeng@douban.com>
2012-07-26 11:17:23 +0800

    add get_pinyin_all and use class design

--- pinyin/__init__.py
+++ pinyin/__init__.py
@@ -7,35 +7,82 @@ import mmseg
 import itertools
 from version import __version__
 
-__all__ = ["get_pinyin", "__version__"]
+__all__ = ["Pinyin", "__version__"]
 
 base = os.path.abspath(os.path.dirname(__file__))
-mmseg.mmseg.dict_load_words(base + "/data/mmseg/words-pytrans.dic")
 zh_cn = re.compile(ur'^[\u4e00-\u9fa5]+$')
 
-dic_default_py = dict(tuple(l.strip().split(',')) for l in open(
-                    base + "/data/pinyin/pinyin.dic"))
-dic_mul_py = {}
-for line in  open(base + "/data/pinyin/wz_py.dic"):
-    wz, pyl = line.strip().split('#')
-    pys = pyl.split('@')
-    dic_mul_py[wz] = pys
+class Pinyin:
+    """拼音(音字)转换"""
 
-def _pinyin(wz):
-    py  = dic_mul_py.get(wz, None)
-    if not py:
-        py = [dic_default_py.get(w.encode("utf-8"), w) 
-                for w in wz.decode("utf-8")]
-    return py
+    def __init__(self):
+        # for get_pinyin_all
+        self.hanpin = {}
+        for line in open(base + "/data/pinyin/allpinyin.dic"):
+            py,words = self._split_word(line)
+            for item in words:
+                self.hanpin.setdefault(item, []).append(py)
 
-def get_pinyin(text):
-    if type(text) == unicode:
-        text = text.encode("utf-8")
-    wl = mmseg.seg_txt(text)
-    pyl = [zh_cn.match(wz.decode("utf-8")) and _pinyin(wz) or wz.decode('utf-8') for wz in wl ]
-    return list(itertools.chain.from_iterable(pyl))
+        # for get_pinyin
+        mmseg.mmseg.dict_load_words(base + "/data/mmseg/words-pytrans.dic")
+
+        self.dic_default_py = dict(tuple(l.strip().split(',')) 
+                for l in open(base + "/data/pinyin/pinyin.dic"))
+        self.dic_mul_py = {}
+        for line in  open(base + "/data/pinyin/wz_py.dic"):
+            wz, pyl = line.strip().split('#')
+            pys = pyl.split('@')
+            self.dic_mul_py[wz] = pys
+
+    def _split_word(self, line):
+        line = line.strip().decode('utf-8')
+        py,content = line.split(" ")
+        words = [w for w in content]
+        return py,words
+
+    def _perm(self,l):
+        if not l:
+            yield []
+        else:
+            for i in l[0]:
+                for j in self._perm(l[1:]):
+                    yield [i] + j
+
+    def get_pinyin_all(self, text):
+        """返回这个词所有可能的拼音."""
+        if not isinstance(text, unicode):
+            text = text.decode('utf-8', 'ignore')
+        rs = [self.hanpin.get(w,[w]) for w in text]
+        if rs and max(len(w) for w in rs) > 1:
+            return list(self._perm(rs))
+        else:
+            return [[w[0] for w in rs]]
+
+    def _pinyin(self, wz):
+        py  = self.dic_mul_py.get(wz, None)
+        if not py:
+            py = [self.dic_default_py.get(w.encode("utf-8"), w) 
+                    for w in wz.decode("utf-8")]
+        return py
+
+    def get_pinyin(self, text):
+        if type(text) == unicode:
+            text = text.encode("utf-8", 'ignore')
+        wl = mmseg.seg_txt(text)
+        pyl = [zh_cn.match(wz.decode("utf-8",'ignore')) 
+                and self._pinyin(wz) or wz.decode('utf-8','ignore') 
+                for wz in wl ]
+        return list(itertools.chain.from_iterable(pyl))
 
 if __name__ == "__main__":
-    wl = get_pinyin("我是中国银行门口的行人， 我要去工行办事请， 她是个心宽体胖的大胖子我会随便说么")
-    for py in wl:
-        print py
+    py = Pinyin()
+    text = "小飞人卡尔松hh哈哈银行"
+    print "Pinyin:get_pinyin---", text
+    for i in py.get_pinyin(text):
+        print i,
+    print "\n"
+    print "Pinyin:get_pinyin_all---", text
+    for i in py.get_pinyin_all(text):
+        for j in i:
+            print j,
+        print 
